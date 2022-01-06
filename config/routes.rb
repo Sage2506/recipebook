11ffd9071a7007e87ffe644b1c4931f9ca52de29
Eduardo Salazar <german2506.gs@gmail.com>
2022-01-06 11:18:06 -0700

    sign up working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :ingredients
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   resources :dishes
   authenticated do
     root to: "dishes#index", as: :authenticated_root
