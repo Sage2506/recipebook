405e1a07e9b2ac9746f1d11ddf6fff468ac76d6f
Eduardo Salazar <german2506.gs@gmail.com>
2022-01-05 18:10:03 -0700

    created ingredient user and dishes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :ingredients
+  devise_for :users
   resources :dishes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
