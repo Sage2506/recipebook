e194a1a5f26ca5b011dd7cf1cdf3bb26328b5e6a
Eduardo Salazar <german2506.gs@gmail.com>
2022-01-31 10:29:08 -0700

    turbo stream delete dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -60,7 +60,7 @@ class DishesController < ApplicationController
   def destroy
     @dish.destroy
     respond_to do |format|
-      format.html { redirect_to dishes_url, notice: 'Dish was successfully destroyed.' }
+      format.html { redirect_to dishes_url, notice: 'Dish was successfully destroyed.', status: :see_other }
       format.json { head :no_content }
     end
   end
